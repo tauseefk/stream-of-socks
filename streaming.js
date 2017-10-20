97be70b4bb3594b88d4790d17ec919095e49e7eb
Tauseef Khan <tauseefk@gmail.com>
2017-10-20 13:03:09 -0400

    renaming stuff

--- streaming.js
+++ streaming.js
@@ -1,14 +1,14 @@
 'use strict'
-var sockStream2 = new toyRx.Stream(function(observer) {
+var sockStream = new toyRx.Stream(function(observer) {
   socket.on('newElement', observer.next);
   socket.on('disconnect', observer.complete);
 })
 
-var blip2 = sockStream2.map(function(data) {
+var blip = sockStream.map(function(data) {
   return data.hello;
 });
 
-blip2.subscribe(
+blip.subscribe(
   {
     next: function(data) {
       document.querySelector('#counter').textContent++;
