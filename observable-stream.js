6a35d46f37bc9ce6bf0ddbe386a41756b915528f
Tauseef Khan <tauseefk@gmail.com>
2017-11-02 19:41:48 -0400

    retype

--- observable-stream.js
+++ observable-stream.js
@@ -12,10 +12,10 @@ var toyRx = (function (){
 
   Observable.prototype.subscribe = function(observer) {}
 
-/**
- * Stream @extends Observable
- * @param subscribe: 
- */
+  /**
+   * Stream @extends Observable
+   * @param subscribe:
+   */
   var Stream = function(subscribe) {
     Observable.call(this, subscribe);
   }
@@ -23,33 +23,32 @@ var toyRx = (function (){
   Stream.prototype = Object.create(Observable.prototype);
   Stream.prototype.constructor = Stream;
 
+  Stream.prototype.map = function(fn) {
+    return this.compose(map(fn));
+  }
+
+  Stream.prototype.compose = function(operator) {
+    operator(this);
+  }
+
   /**
-  * @param fn: a function that takes an observable
-  * @returns observable with observer mapped to fn
-  */
+   * @param fn: a function that takes an observable
+   * @returns observable with observer mapped to fn
+   */
   var map = function(fn) {
     return function(stream) {
       return {
         subscribe: function(observer) {
           stream.subscribe({
             next: function(value) {
-              return observer.next(fn(value))
+              return observer.next(fn(value));
             },
             complete: observer.complete
-          })
+          });
         }
       }
     }
   }
-
-  Stream.prototype.compose = function(operator) {
-    return operator(this);
-  }
-
-  Stream.prototype.map = function(fn) {
-    return this.compose(map(fn));
-  }
-
 
   return {
     Stream: Stream
